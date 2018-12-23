3ab7e129766c61d4de0cdf63784fab3b83c1adac
James Stewman <jamiebum12@live.com>
2018-12-23 02:26:19 -0900

    reset to scratch

--- js/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World");
-
-let dontClickButton = document.getElementById('dont-click');
-
-dontClickButton.addEventListener('click', function(){
-    alert('I said don\'t click');
-});
