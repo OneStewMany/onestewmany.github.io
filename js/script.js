6931a5ceb602e07798f6820cec2543bead87224c
OneStewMany <jamiebum12@live.com>
2018-12-22 15:34:10 -0900

    fixed missing semicolon

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ console.log("Hello World");
 var button = document.getElementById('dont-click-button');
 
 button.addEventListener('click', function(){
-    alert('I said don\'t click me')
+    alert('I said don\'t click me');
 })
